8c6abac977d0a819a1436f0793890abcc9426a15
adeka stanley <adekastanley2@gmail.com>
2025-01-09 14:31:08 +0100

    new

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<p className="bg-red-500 text-center mt-20 text-bold">
+				Under construction
+			</p>
+		</>
+	);
+}
+
+export default App;
