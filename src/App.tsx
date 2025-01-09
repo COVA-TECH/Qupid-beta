b03d633168e898318366e01521597caadf4451c2
adeka stanley <adekastanley2@gmail.com>
2025-01-09 14:46:28 +0100

    first dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<p className="bg-red-500 text-center mt-20 text-bold">
-				Under construction
-			</p>
+			<p className="bg-red-500 text-center mt-20 text-bold">being built</p>
 		</>
 	);
 }
